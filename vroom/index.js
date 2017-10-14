45e9f5817e9432620e0786e8c0979a4f2d551f77
Connick Shields <connickshields@gmail.com>
2017-10-13 20:29:45 -0700

    added Nunito font, added color constants
    Globals.js has color costants

--- vroom/index.js
+++ vroom/index.js
@@ -1,4 +1,57 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-AppRegistry.registerComponent('vroom', () => App);
+GLOBAL = require('./src/Globals');
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class vroom extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to Vroom!
+        </Text>
+        <Text style={styles.instructions}>
+          powered by React Native
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: GLOBAL.COLOR.GRAY,
+  },
+  welcome: {
+    fontFamily: 'Nunito',
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: GLOBAL.COLOR.BLUE,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    fontFamily: 'Nunito',
+    textAlign: 'center',
+    color: GLOBAL.COLOR.DARKBLUE,
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('vroom', () => vroom);
---
+++ vroom/src/Globals.js
@@ -0,0 +1,11 @@
+module.exports = {
+  COLOR: {
+    BLUE: '#C2DFE3',
+    DARKBLUE: '#E0FBFC',
+    GRAY: '#4F6D7A',
+    DARKGRAY: '#253237',
+    RED: '#FF3C38',
+    GREEN: '#9FD356',
+    YELLOW: '#FDC939'
+  }
+};
