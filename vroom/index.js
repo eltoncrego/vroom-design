f2deccc3ae985c3efa57afdea118062c7b3e52f5
Connick Shields <connickshields@gmail.com>
2017-10-21 15:57:31 -0700

    remove unused import

--- vroom/index.js
+++ vroom/index.js
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 import Login from './src/components/Login/Login';
 import Onboarding from './src/components/Onboarding/Onboarding';
 import Revi from './src/Revi'
-import { 
-  AppRegistry, 
-  NavigatorIOS, 
+import {
+  AppRegistry,
   StyleSheet,
 } from 'react-native';
 import {
@@ -16,14 +15,14 @@ GLOBAL = require('./src/Globals');
 /*
  * Constant: Vroom
  * Author: Elton C. Rego
- * 
+ *
  * Purpose: Creates a stack of the possible screens
  *   that can be used in the application
  */
 const vroom = StackNavigator({
   // DEBUG: Uncomment Following Line to Test Revi
   // Revi: {screen: Revi},
-  
+
   Login: {screen: Login},
   Onboarding: {screen: Onboarding},
 });
