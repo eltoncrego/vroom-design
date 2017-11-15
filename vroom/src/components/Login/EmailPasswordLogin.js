a34b55503d124a9d1d902279fb30385b68fd28d6
Elton Rego <rego.elton@gmail.com>
2017-11-15 13:57:36 -0800

    Fixed drawer glitch on screens that didn't need drawer.

--- vroom/src/components/Login/EmailPasswordLogin.js
+++ vroom/src/components/Login/EmailPasswordLogin.js
@@ -56,6 +56,7 @@ export default class EmailPasswordLogin extends Component {
   static navigationOptions = {
     title: 'EmailPasswordLogin',
     header: null,
+    drawerLockMode: 'locked-closed',
   };
 
   // Author: Alec Felt, Connick Shields
