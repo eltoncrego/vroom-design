75c27c0f68f5365a685de5d1e478e2ad1eb52858
KotovSyndrome <pkatoozian@Payams-MacBook-Pro.local>
2017-11-18 18:24:03 -0800

    User can now confirm or cancel deletion of their account

--- vroom/src/components/Database/Settings.js
+++ vroom/src/components/Database/Settings.js
@@ -14,6 +14,7 @@ import {
   Text,
   ScrollView,
   TouchableOpacity,
+  Alert
 } from 'react-native';
 
 // Files Needed
@@ -57,8 +58,14 @@ export default class Settings extends Component {
   }
 
   deleteAccount() {
-    alert("This action cannot be undone");
-    deleteUser();
+    Alert.alert(
+      'Confirm Deletion',
+      'Are you sure you want to delete your account?',
+      [
+        {text: "Yes", onPress: () => deleteUser()},
+        {text: "No"},
+      ]
+    )
   }
 
 
